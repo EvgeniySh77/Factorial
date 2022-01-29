949110d5c91aa515814e5ea4df3e1145b026b0f2
EvgeniySh77 <evgen.sh77@mail.ru>
2022-01-29 17:12:22 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 20;
+            Console.WriteLine(Factorial(x));
+
+            Console.ReadKey();
+        }
+
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+    }
+}
