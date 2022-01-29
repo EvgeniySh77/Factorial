09e0085508e50fed7742759ff5257351b862f2ed
EvgeniySh77 <evgen.sh77@mail.ru>
2022-01-29 17:27:28 +0300

    Добавлен метод возведения в степень

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace Factorial
         static void Main()
         {
             int x = 20;
-            Console.WriteLine(Factorial(x));
+            Console.WriteLine($"Факториал числа {x}\t" + Factorial(x));
+
+            int n = 2;
+            byte pow = 8;
+            Console.WriteLine($"Число {n} возведенное в степень {pow}\t" + PowerUp(n, pow));
 
             Console.ReadKey();
         }
@@ -21,6 +25,25 @@ namespace Factorial
             else
             {
                 return x * Factorial(x - 1);
+            }
+        }
+
+        public static int PowerUp(int n, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return n;
+                }
+                else
+                {
+                    return n * PowerUp(n, --pow);
+                }
             }
         }
     }
